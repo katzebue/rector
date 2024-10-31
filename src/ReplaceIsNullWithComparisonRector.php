990a4211bda1d5ca10af3a794861050f3c47bee6
Julia Berezovskaya <katzebue@gmail.com>
2024-10-31 16:49:14 +0300

    ReplaceIsNullWithComparisonRector - `!is_null` support

--- src/ReplaceIsNullWithComparisonRector.php
+++ src/ReplaceIsNullWithComparisonRector.php
@@ -7,6 +7,8 @@ namespace Katzebue\Rector;
 use Override;
 use PhpParser\Node;
 use PhpParser\Node\Expr\BinaryOp\Identical;
+use PhpParser\Node\Expr\BinaryOp\NotIdentical;
+use PhpParser\Node\Expr\BooleanNot;
 use PhpParser\Node\Expr\FuncCall;
 use Rector\Rector\AbstractRector;
 use Symplify\RuleDocGenerator\ValueObject\CodeSample\CodeSample;
@@ -17,30 +19,33 @@ final class ReplaceIsNullWithComparisonRector extends AbstractRector
     #[Override]
     public function getRuleDefinition(): RuleDefinition
     {
-        return new RuleDefinition('Replace is_null() with === null', [
+        return new RuleDefinition('Replace is_null() with === null or !== null', [
             new CodeSample('is_null($value);', '$value === null;'),
+            new CodeSample('!is_null($value);', '$value !== null;'),
         ]);
     }
 
     #[Override]
     public function getNodeTypes(): array
     {
-        return [FuncCall::class];
+        return [FuncCall::class, BooleanNot::class];
     }
 
     #[Override]
     public function refactor(Node $node): ?Node
     {
-        if (!$this->isName($node, 'is_null')) {
-            return null;
+        // !is_null($value)
+        if ($node instanceof BooleanNot && $node->expr instanceof FuncCall && $this->isName($node->expr, 'is_null')) {
+            $arg = $node->expr->args[0]->value;
+            return new NotIdentical($arg, $this->nodeFactory->createNull());
         }
 
-        if (!isset($node->args[0])) {
-            return null;
+        // is_null($value)
+        if ($node instanceof FuncCall && $this->isName($node, 'is_null')) {
+            $arg = $node->args[0]->value;
+            return new Identical($arg, $this->nodeFactory->createNull());
         }
 
-        $arg = $node->args[0]->value;
-
-        return new Identical($arg, $this->nodeFactory->createNull());
+        return null;
     }
 }
